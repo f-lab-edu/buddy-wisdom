712daf0f4e53e9653cd8c750bcaa37060e71663b
SUA <haemahmtea@gmail.com>
2023-04-25 23:12:34 +0900

    initial setting

---
+++ src/main/java/cobook/buddywisdom/BuddyWisdomApplication.java
@@ -0,0 +1,13 @@
+package cobook.buddywisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyWisdomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddyWisdomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cobook/buddywisdom/BuddyWisdomApplicationTests.java
@@ -0,0 +1,8 @@
+package cobook.buddywisdom;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyWisdomApplicationTests {
+
+}
