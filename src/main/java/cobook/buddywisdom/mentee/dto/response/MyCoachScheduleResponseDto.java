4c553bf1148af2834950a1786da1a0701ec68fd9
keemsora <itsyoursora@gmail.com>
2023-05-15 12:23:37 +0900

    [#10] :sparkles: 코칭 스케줄 조회 DTO 추가

---
+++ src/main/java/cobook/buddywisdom/mentee/dto/response/MyCoachScheduleResponseDto.java
@@ -0,0 +1,21 @@
+package cobook.buddywisdom.mentee.dto.response;
+
+import java.time.LocalDateTime;
+
+import cobook.buddywisdom.coach.domain.CoachSchedule;
+
+public record MyCoachScheduleResponseDto(
+	Long id,
+	Long coachId,
+	LocalDateTime possibleDateTime,
+	boolean matchYn
+) {
+	public static MyCoachScheduleResponseDto from(CoachSchedule coachSchedule) {
+		return new MyCoachScheduleResponseDto(
+			coachSchedule.getId(),
+			coachSchedule.getCoachId(),
+			coachSchedule.getPossibleDateTime(),
+			coachSchedule.isMatchYn()
+		);
+	}
+}
