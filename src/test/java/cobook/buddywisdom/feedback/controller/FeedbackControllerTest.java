353374069864d54515ff0fb11be8ddfac56d6d5e
keemsora <itsyoursora@gmail.com>
2023-05-31 11:17:40 +0900

    [#27] :white_check_mark: 분기문 처리 layer 변경 적용

--- src/test/java/cobook/buddywisdom/feedback/controller/FeedbackControllerTest.java
+++ src/test/java/cobook/buddywisdom/feedback/controller/FeedbackControllerTest.java
@@ -1,5 +1,6 @@
 package cobook.buddywisdom.feedback.controller;
 
+import static cobook.buddywisdom.global.vo.MemberApiType.*;
 import static org.mockito.ArgumentMatchers.*;
 
 import java.util.Optional;
@@ -12,7 +13,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.BDDMockito;
 import org.mybatis.spring.boot.test.autoconfigure.AutoConfigureMybatis;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -23,14 +23,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import cobook.buddywisdom.feedback.domain.Feedback;
 import cobook.buddywisdom.feedback.dto.FeedbackResponseDto;
 import cobook.buddywisdom.feedback.dto.UpdateFeedbackRequestDto;
 import cobook.buddywisdom.feedback.service.FeedbackService;
-import cobook.buddywisdom.global.vo.MemberApiType;
 import cobook.buddywisdom.util.WithMockCustomUser;
 
 @AutoConfigureMybatis
@@ -78,25 +76,45 @@ public class FeedbackControllerTest {
 	@WithMockCustomUser
 	@DisplayName("피드백 확인")
 	class UpdateFeedbackTest {
-		@ParameterizedTest
-		@ValueSource(strings = {"coaches", "mentees"})
-		@DisplayName("유효한 피드백 정보가 전달되면 메서드를 호출하고 200 OK를 반환한다.")
-		void when_requestInformationIsValid_expect_callMethodAndReturn200Ok(String memberApiType) throws Exception {
-			UpdateFeedbackRequestDto request = new UpdateFeedbackRequestDto(1L, "피드백");
+		@Test
+		@DisplayName("유효한 멘티의 피드백 정보가 전달되면 메서드를 호출하고 200 OK를 반환한다.")
+		void when_menteeRequestInformationIsValid_expect_callMethodAndReturn200Ok() throws Exception {
+			UpdateFeedbackRequestDto request = new UpdateFeedbackRequestDto(1L, "멘티 피드백");
 
 			BDDMockito.willDoNothing()
-				.given(feedbackService).updateFeedback(anyLong(), anyString(), any(MemberApiType.class));
+				.given(feedbackService).updateFeedbackByMentee(anyLong(), anyString());
 
 			ResultActions response =
 				mockMvc.perform(
-					MockMvcRequestBuilders.patch(BASE_URL + memberApiType + "/feedback")
+					MockMvcRequestBuilders.patch(BASE_URL + MENTEES + "/feedback")
 						.with(SecurityMockMvcRequestPostProcessors.csrf())
 						.contentType(MediaType.APPLICATION_JSON)
 						.content(objectMapper.writeValueAsString(request)))
 				.andDo(MockMvcResultHandlers.print());
 
-			BDDMockito.verify(feedbackService).updateFeedback(anyLong(), anyString(), any(MemberApiType.class));
+			BDDMockito.verify(feedbackService).updateFeedbackByMentee(anyLong(), anyString());
 			response.andExpect(MockMvcResultMatchers.status().isNoContent());
 		}
 	}
+
+	@Test
+	@DisplayName("유효한 코치의 피드백 정보가 전달되면 메서드를 호출하고 200 OK를 반환한다.")
+	void when_coachRequestInformationIsValid_expect_callMethodAndReturn200Ok() throws Exception {
+		UpdateFeedbackRequestDto request = new UpdateFeedbackRequestDto(1L, "코치 피드백");
+
+		BDDMockito.willDoNothing()
+			.given(feedbackService).updateFeedbackByCoach(anyLong(), anyString());
+
+		ResultActions response =
+			mockMvc.perform(
+					MockMvcRequestBuilders.patch(BASE_URL + COACHES + "/feedback")
+						.with(SecurityMockMvcRequestPostProcessors.csrf())
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(request)))
+				.andDo(MockMvcResultHandlers.print());
+
+		BDDMockito.verify(feedbackService).updateFeedbackByCoach(anyLong(), anyString());
+		response.andExpect(MockMvcResultMatchers.status().isNoContent());
+	}
 }
+
--- src/test/java/cobook/buddywisdom/feedback/service/FeedbackServiceTest.java
+++ src/test/java/cobook/buddywisdom/feedback/service/FeedbackServiceTest.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -21,7 +19,6 @@ import cobook.buddywisdom.feedback.domain.Feedback;
 import cobook.buddywisdom.feedback.dto.FeedbackResponseDto;
 import cobook.buddywisdom.feedback.exception.NotFoundFeedbackException;
 import cobook.buddywisdom.feedback.mapper.FeedbackMapper;
-import cobook.buddywisdom.global.vo.MemberApiType;
 import cobook.buddywisdom.util.WithMockCustomUser;
 
 @ExtendWith(MockitoExtension.class)
@@ -73,8 +70,8 @@ public class FeedbackServiceTest {
 	@DisplayName("피드백 확인")
 	class UpdateFeedbackTest {
 		@Test
-		@DisplayName("피드백 내역이 존재하면 apiType이 mentees일 때 멘티 피드백을 추가한다.")
-		void when_feedbackExistsAndApiTypeIsMentees_expect_updateFeedbackByApiType() {
+		@DisplayName("피드백 내역이 존재하면 멘티 피드백을 추가한다.")
+		void when_feedbackExists_expect_updateFeedbackByMentee() {
 			Feedback feedback = Feedback.of(1L, null, null);
 
 			BDDMockito.given(feedbackMapper.findByMenteeScheduleId(anyLong()))
@@ -82,14 +79,25 @@ public class FeedbackServiceTest {
 			BDDMockito.willDoNothing()
 				.given(feedbackMapper).updateMenteeFeedbackByMenteeScheduleId(anyLong(), anyString());
 
-			feedbackService.updateFeedback(1L, "멘티 피드백", MemberApiType.MENTEES);
+			feedbackService.updateFeedbackByMentee(1L, "멘티 피드백");
 
 			BDDMockito.verify(feedbackMapper)
 				.updateMenteeFeedbackByMenteeScheduleId(1L, "멘티 피드백");
 		}
 
 		@Test
-		@DisplayName("피드백 내역이 존재하면 apiType이 coaches일 때 멘티 피드백을 추가한다.")
+		@DisplayName("피드백 내역이 존재하지 않으면 NotFoundFeedbackException을 반환한다.")
+		void when_feedbackDoesNotExistsForMentee_expect_throwsNotFoundFeedbackException() {
+			BDDMockito.given(feedbackMapper.findByMenteeScheduleId(anyLong()))
+				.willReturn(Optional.empty());
+
+			AssertionsForClassTypes.assertThatThrownBy(() ->
+					feedbackService.updateFeedbackByMentee(1L, "멘티 피드백"))
+				.isInstanceOf(NotFoundFeedbackException.class);
+		}
+
+		@Test
+		@DisplayName("피드백 내역이 존재하면 apiType이 coaches일 때 코치 피드백을 추가한다.")
 		void when_feedbackExistsAndApiTypeIsCoaches_expect_updateFeedbackByApiType() {
 			Feedback feedback = Feedback.of(1L, null, null);
 
@@ -98,22 +106,21 @@ public class FeedbackServiceTest {
 			BDDMockito.willDoNothing()
 				.given(feedbackMapper).updateCoachFeedbackByMenteeScheduleId(anyLong(), anyString());
 
-			feedbackService.updateFeedback(1L, "코치 피드백", MemberApiType.COACHES);
+			feedbackService.updateFeedbackByCoach(1L, "코치 피드백");
 
 			BDDMockito.verify(feedbackMapper)
 				.updateCoachFeedbackByMenteeScheduleId(1L, "코치 피드백");
 		}
 
-		@ParameterizedTest
-		@ValueSource(strings = {"MENTEES", "COACHES"})
+		@Test
 		@DisplayName("피드백 내역이 존재하지 않으면 NotFoundFeedbackException을 반환한다.")
-		void when_feedbackDoesNotExists_expect_throwsNotFoundFeedbackException(String apiType) {
+		void when_feedbackDoesNotExistsForCoach_expect_throwsNotFoundFeedbackException() {
 			BDDMockito.given(feedbackMapper.findByMenteeScheduleId(anyLong()))
 				.willReturn(Optional.empty());
 
 			AssertionsForClassTypes.assertThatThrownBy(() ->
-				feedbackService.updateFeedback(1L, "피드백", MemberApiType.valueOf(apiType)))
-			.isInstanceOf(NotFoundFeedbackException.class);
+					feedbackService.updateFeedbackByCoach(1L, "코치 피드백"))
+				.isInstanceOf(NotFoundFeedbackException.class);
 		}
 	}
 }
